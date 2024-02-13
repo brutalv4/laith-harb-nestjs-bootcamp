cdabbe53bf3eabd45e1f9c98446f42e26e67945f
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-02-13 15:46:14 +0200

    54. Storing the User in the Database

--- realtor-app/src/user/auth/auth.service.ts
+++ realtor-app/src/user/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { UserType } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 import { SignupDto } from '../dtos/auth.dto';
 import { PrismaService } from './../../prisma/prisma.service';
@@ -7,11 +9,26 @@ import { PrismaService } from './../../prisma/prisma.service';
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email }: SignupDto) {
-    const user = await this.prismaService.user.findUnique({ where: { email } });
+  async signup({ email, password, name, phone }: SignupDto) {
+    const userExists = !!(await this.prismaService.user.findUnique({
+      where: { email },
+    }));
 
-    if (user) {
+    if (userExists) {
       throw new ConflictException('user with given email already exits');
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
