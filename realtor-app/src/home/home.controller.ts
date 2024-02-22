3d4dc51f9bdee80e64d73a484abda1fd46eda2f5
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-02-22 11:24:50 +0200

    61. Fetching All Homes

--- realtor-app/src/home/home.controller.ts
+++ realtor-app/src/home/home.controller.ts
@@ -9,11 +9,16 @@ import {
   Put,
 } from '@nestjs/common';
 
+import { HomeResponseDto } from './dtos/home.dto';
+import { HomeService } from './home.service';
+
 @Controller('home')
 export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+
   @Get()
-  getHomes() {
-    return [];
+  getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
   }
 
   @Get(':id')
