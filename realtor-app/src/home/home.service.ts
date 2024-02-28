92e54e29670b04e9ede41149ef89f488961b645f
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-02-28 15:25:34 +0200

    62. Define the Response DTO

--- realtor-app/src/home/home.service.ts
+++ realtor-app/src/home/home.service.ts
@@ -8,7 +8,27 @@ export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getHomes(): Promise<HomeResponseDto[]> {
-    const homes = await this.prismaService.home.findMany();
-    return homes.map((home) => new HomeResponseDto(home));
+    const homes = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        property_type: true,
+        number_of_bathrooms: true,
+        number_of_bedrooms: true,
+        images: {
+          select: {
+            url: true,
+          },
+          take: 1,
+        },
+      },
+    });
+    return homes.map((home) => {
+      const next = { ...home, image: home.images[0].url };
+      delete next.images;
+      return new HomeResponseDto(next);
+    });
   }
 }
